224ac74caf57853148f982344a7be88b0ee45c2d
Sciapan Hembar <hsciapan@gmail.com>
2024-07-01 16:42:40 +0300

    P-3: add news table and create news table script

---
+++ src/main/java/com/solbeg/nnewsservice/entity/News.java
@@ -0,0 +1,32 @@
+package com.solbeg.nnewsservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "news")
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime time;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String text;
+
+}
