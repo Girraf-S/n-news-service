d6b80a653a0a489f77d1e71637aebab54965a4fb
Sciapan Hembar <hsciapan@gmail.com>
2024-07-01 15:03:51 +0300

    P-3: init project

---
+++ src/main/java/com/solbeg/nnewsservice/NNewsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.solbeg.nnewsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NNewsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NNewsServiceApplication.class, args);
+    }
+
+}
