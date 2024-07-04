475a71b1aa1d4357692fae4e860fc395393e1243
Sciapan Hembar <hsciapan@gmail.com>
2024-07-04 19:27:09 +0300

    P-3: Add CreationTimestamp to News entity

--- src/main/java/com/solbeg/nnewsservice/entity/News.java
+++ src/main/java/com/solbeg/nnewsservice/entity/News.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -21,6 +22,7 @@ public class News {
     private Long id;
 
     @Column(nullable = false)
+    @CreationTimestamp
     private LocalDateTime time;
 
     @Column(nullable = false)
